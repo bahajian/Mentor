7957189575b8d9011f0cd774192ca98e3115c95d
bahajian <bahajian@gmail.com>
2021-02-09 00:19:56 -0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "mysql": {
         connectionLimit : 100,
-        host     : 'mentor-db.c3xnishtyqbf.ca-central-1.rds.amazonaws.com',
+        host     : 'restful-api.c3xnishtyqbf.ca-central-1.rds.amazonaws.com',
         user     : 'admin',
         password : '10021354',
         database : 'restful-api',
